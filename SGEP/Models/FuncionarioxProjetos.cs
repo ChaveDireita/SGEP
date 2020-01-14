9c53cff10fe207363fbcab16e1fcb3f909c5ac31
Daniel <revolverocelot.r3v0lv3r0c3l0t@gmail.com>
2020-01-13 21:18:26 -0300

    Início da alocação

---
+++ SGEP/Models/FuncionarioxProjetos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGEP.Models
+{
+    public class FuncionarioxProjetos
+    {
+        public Funcionario FunAlocado { get; set; }
+        public Projeto ProjReceptor { get; set; }
+    }
+}
