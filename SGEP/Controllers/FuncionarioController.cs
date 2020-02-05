2eaea831312a937c8412de7561331bd082545247
Paulo Sérgio Pinheiro Gramosa Silva <paulo.sergiopnhr161@hotmail.com>
2020-02-05 13:20:18 -0300

    Olha aê Nolas.

--- SGEP/Controllers/FuncionarioController.cs
+++ SGEP/Controllers/FuncionarioController.cs
@@ -55,7 +55,6 @@ namespace SGEP.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Nome,Cargo")] Funcionario funcionario)
         {
             if (ModelState.IsValid)
