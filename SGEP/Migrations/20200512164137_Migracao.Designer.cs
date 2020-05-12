69fb86ae2623eb6c6b8fc6b6f6b71931a1bb0c72
ChaveDireita <geometrianaoeuclidiana@gmail.com>
2020-05-12 14:47:38 -0300

    fix

--- SGEP/Migrations/20200512164137_Migracao.Designer.cs
+++ SGEP/Migrations/20200512164137_Migracao.Designer.cs
@@ -10,7 +10,7 @@ using SGEP.Data;
 namespace SGEP.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20200509002740_Migracao")]
+    [Migration("20200512164137_Migracao")]
     partial class Migracao
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
